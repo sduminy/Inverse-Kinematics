1319463c0535ac1a8c1e416bdee2f44bd7d26584
sduminy <sduminy@laas.fr>
2019-12-11 16:44:53 +0100

    New foot trajectory using Bezier curve

--- ftraj_bezier.py
+++ ftraj_bezier.py
@@ -34,20 +34,28 @@ N_DISCRETIZATION = 100
 Vdesired = 0.04		# desired velocity of the robot
 L = 0.04			# desired leg stride in a gait cycle
 Tst = L/Vdesired	# time of the stance phase
-vD = 0.005			# virtual displacement to ensure the body stability
+vD = 0.001			# virtual displacement to ensure the body stability
 
 Tsw = 0.25			# time of the swing phase
 T = Tst + Tsw		# time of one gait cycle
-h = 0.06			# maximal height of the foot
+h = 0.03			# maximal height of the foot
 
 # Bezier curves for the swing phase
 
-P0 = [0., 0.]
-P1 = [L/2, h]
-P2 = [L, 0.]
+P0  = [   0., 0. ]
+P1  = [-0.005, 0. ]
+P2  = [-0.02, 0.02]
+P3  = [-0.02, 0.02]
+P4  = [-0.02, 0.02]
+P5  = [ 0.02, 0.02]
+P6  = [ 0.02, 0.02]
+P7  = [ 0.02, 0.03]
+P8  = [ 0.06, 0.03]
+P9  = [ 0.06, 0.03]
+P10 = [ 0.045, 0. ]
+P11 = [    L, 0. ]
 
-waypoints = matrix([P0, P1, P2]).T 
-
+waypoints = matrix([P0, P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11]).T
 bc = bezier(waypoints, 0.0, Tsw)
 
 dt = np.linspace(0., Tsw, N_DISCRETIZATION)
@@ -60,21 +68,7 @@ bcZ = []
 for i in dt: 
 	bcX.append(bc(i)[0,0]) 
 	bcZ.append(bc(i)[1,0])
-"""
-def ftraj_bezier(DeltaT, P0):
-	x = []
-	z = []
-	for i,t in enumerate(DeltaT):
-		t %= T
-		if i < N_DISCRETIZATION :
-			x.append(bcX[i])
-			z.append(bcZ[i])
-		else :
-			t -= Tsw
-			x.append(P2[0] - Vdesired * t)
-			z.append(-vD * cos(pi * (0.5 - Vdesired * t / L)) - P0[1])
-	return x,z
-"""
+
 def ftraj_swing(DeltaT, P0):
 	x = []
 	z = []
@@ -92,12 +86,23 @@ def ftraj_stance(DeltaT, P0):
 		t %= T
 		if t > Tsw:
 			t -= Tsw
-			x.append(Vdesired * t)
+			x.append(P11[0] - Vdesired * t)
 			z.append(-vD * cos(pi * (0.5 - Vdesired * t / L)) - P0[1])
 	return x,z
 
-
-
+def ftraj(DeltaT, P0):
+	x = []
+	z = []
+	for i,t in enumerate(DeltaT):
+		t %= T
+		if i < N_DISCRETIZATION :
+			x.append(bcX[i])
+			z.append(bcZ[i])
+		if t > Tsw:
+			t -= Tsw
+			x.append(P11[0] - Vdesired * t)
+			z.append(-vD * cos(pi * (0.5 - Vdesired * t / L)) - P0[1])
+	return x,z
 
 N_SIMULATION = int(N_DISCRETIZATION * T / Tsw)
 
@@ -106,23 +111,35 @@ DeltaT = np.linspace(0., T, N_SIMULATION)
 Xsw, Zsw = ftraj_swing(DeltaT, P0)
 Xst, Zst = ftraj_stance(DeltaT, P0)
 
-plt.figure()
-plt.plot(Xsw, Zsw, 'g')
-plt.plot(Xst, Zst, 'r')
-plt.show()
-
-"""
-X,Z = ftraj_bezier(DeltaT, P0)
+X,Z = ftraj(DeltaT, P0)
 
 plt.figure(1)
-plt.plot(X,Z)
+plt.plot(Xsw, Zsw, 'g')
+plt.plot(Xst, Zst, 'r')
+plt.plot(P0[0], P0[1], 'x', label="control points")
+plt.plot(P1[0], P1[1], 'x')
+plt.plot(P2[0], P2[1], 'x')
+plt.plot(P3[0], P3[1], 'x')
+plt.plot(P4[0], P4[1], 'x')
+plt.plot(P5[0], P5[1], 'x')
+plt.plot(P6[0], P6[1], 'x')
+plt.plot(P7[0], P7[1], 'x')
+plt.plot(P8[0], P8[1], 'x')
+plt.plot(P9[0], P9[1], 'x')
+plt.plot(P10[0], P10[1], 'x')
+plt.plot(P11[0], P11[1], 'x')
+plt.xlabel('x (m)')
+plt.ylabel('z (m)')
+plt.legend()
+plt.title("Foot trajectory using Bezier curve")
+
 plt.figure(2)
-plt.plot(X[:N_DISCRETIZATION],Z[:N_DISCRETIZATION], 'g')
-plt.plot(X[N_DISCRETIZATION:],Z[N_DISCRETIZATION:], 'r')
-plt.plot(X[N_DISCRETIZATION],Z[N_DISCRETIZATION],'xb')
-plt.plot(X[N_DISCRETIZATION-1],Z[N_DISCRETIZATION-1], '*b') 
+plt.plot(X,Z)
+plt.xlabel('x (m)')
+plt.ylabel('z (m)')
+plt.title("Foot trajectory using Bezier curve")
 plt.show()
-"""
+
 """
 DeltaT = np.linspace(0,2*T,300)
 x0 = -0.19
@@ -135,5 +152,4 @@ plt.ylabel('displacement by z (m)')
 plt.grid()
 plt.title('Parametric curve of the trajectory')
 plt.show()
-
 """
